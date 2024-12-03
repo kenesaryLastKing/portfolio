7918b176772aa5411191c23183083578b02bfcb8
kenesaryLastKing <koni9812@gmail.com>
2024-12-03 16:29:58 +0500

    save

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  background-color: #2350d6;
+  width: 200px;
+  height: 54px;
+  border-radius: 20px;
+  margin-right: 34px;
+  color: #fff;
+  font-size: 16px;
+
+  :hover {
+    background-color: #021c69;
+    transform: translateY(-4px);
+  }
+`
